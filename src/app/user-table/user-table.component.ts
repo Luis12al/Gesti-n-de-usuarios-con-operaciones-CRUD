4151d858ca295a0d6c091360c829a8ab637192dc
Luis12al <ltzmz1246@gmail.com>
2026-03-03 12:14:14 -0500

    Aplicación web desarrollada con Angular, que implementa un sistema de gestión de usuarios con operaciones CRUD (Crear, Leer, Actualizar y Eliminar), integrada en tiempo real con Firebase como base de datos y backend.

--- src/app/user-table/user-table.component.ts
+++ src/app/user-table/user-table.component.ts
@@ -7,6 +7,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],   // ← NECESARIO para *ngFor y *ngIf
   templateUrl: './user-table.component.html',
+  styleUrls: ['./user-table.component.css']
 })
 
 export class UserTableComponent implements OnInit {
